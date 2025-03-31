ec1b6496fd2e3c85b66acaa6d80fdb1d6ee461f8
dkmtech32 <dkmtien123@gmail.com>
2025-03-31 14:00:10 +0700

    fix: correct GrapesJS license key environment variable assignment
    - Updated the assignment of NEXT_PUBLIC_GRAPESJS_LICENSE_KEY to use the correct environment variable for client-side access.

--- src/env.ts
+++ src/env.ts
@@ -10,6 +10,7 @@ export const env = createEnv({
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   runtimeEnv: {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    NEXT_PUBLIC_GRAPESJS_LICENSE_KEY: process.env.GRAPESJS_LICENSE_KEY,
+    NEXT_PUBLIC_GRAPESJS_LICENSE_KEY:
+      process.env.NEXT_PUBLIC_GRAPESJS_LICENSE_KEY,
   },
 });
