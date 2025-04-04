8b5cbf28292da22f2d0b2430c08c993584593349
L3oN <89580650+L3oN007@users.noreply.github.com>
2025-04-04 13:56:44 +0700

    feat(user-tracking): add user tracking hook and types
    - Implemented `useGetUserTracking` hook for fetching user tracking data with pagination and filtering.
    - Added `IUserTracking` and `IUserTrackingResponse` types for better type safety.
    - Introduced `userTrackingQueryKeys` for query key management.
    - Updated `package.json` and `pnpm-lock.yaml` to include `qs` and its types for query string handling.

---
+++ src/types/user-tracking.type.ts
@@ -0,0 +1,38 @@
+export interface IUserTracking {
+  event_name: string;
+  event_data: IEventData;
+  user_agent: string;
+  ip: string;
+  platform: string;
+  language: string;
+  cookies_enabled: boolean;
+  screen_width: number;
+  screen_height: number;
+  timezone: string;
+  timestamp: string;
+  domain: string;
+  uuid: string;
+  path: string;
+  id: {
+    $oid: string;
+  };
+}
+
+export interface IEventData {
+  x?: number;
+  y?: number;
+  mouseMovements?: number;
+  device: string;
+  height?: number;
+  start?: number;
+  end?: number;
+  total?: number;
+  target?: string;
+}
+
+export interface IUserTrackingResponse {
+  success: boolean;
+  data: IPaginatedResponse<IUserTracking>;
+  message: string;
+  type: string;
+}
